ddce5dbc282d5ebd927fc4318f2fe6aaf6f1d465
syedmaaz388 <63224539+syedmaaz388@users.noreply.github.com>
2023-04-30 10:28:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
+  { url: "https://picsum.photos/id/239/200/300" }
 ];
 
 function downloadImages(){
